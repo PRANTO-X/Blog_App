a4b26007b4b2335b5aaf098f46f5bae86d93e54c
PRANTO <mdpranto417@gmail.com>
2025-08-23 20:51:15 +0600

    fix blogDetail loader

--- src/components/blog/BlogDetail.jsx
+++ src/components/blog/BlogDetail.jsx
@@ -51,9 +51,9 @@ const BlogDetail = ({ blogSlug }) => {
           <div className="h-12 bg-gray-300 rounded w-2/3 mb-4"></div>
           <div className="h-64 bg-gray-300 rounded mb-6"></div>
           <div className="space-y-3">
-            <div className="h-4 bg-gray-300 rounded"></div>
-            <div className="h-4 bg-gray-300 rounded"></div>
-            <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+            {Array.from({length: 10}).map((_,index)=>(
+              <div key={index} className={`${index === 9 ? 'w-3/4' : ''} h-4 bg-gray-300 rounded`}></div>
+            ))}
           </div>
         </div>
       </div>
